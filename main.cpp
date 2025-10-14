55b02fe2209936299ac2aca19dadbc04c8dad4c9
Manas <manassoni1311@gmail.com>
2025-10-14 22:59:16 +0530

    Blueprint of this project is added

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Account{};
+
+class Bank{};
+
+void clearScreen(){}
+
+void showAdminDash(Bank &bank){}
+
+void showUserDash(Account* acc){}
+
+int main(){
+    
+    return 0;
+}
